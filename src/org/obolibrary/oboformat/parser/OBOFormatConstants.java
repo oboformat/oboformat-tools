70179478dbb532b742994ad8be1482eabb22062f
shahid.manzoor <shahid.manzoor@3e622410-f0ad-ce59-49ea-9bf623c22f00>
2011-01-05 15:46:00 +0000

    OBO Format Constants are added
    git-svn-id: http://oboformat.googlecode.com/svn/trunk@44 3e622410-f0ad-ce59-49ea-9bf623c22f00

---
+++ src/org/obolibrary/oboformat/parser/OBOFormatConstants.java
@@ -0,0 +1,61 @@
+package org.obolibrary.oboformat.parser;
+
+public class OBOFormatConstants {
+
+	public final static String TAG_ONTOLOGY= "ontology";
+	public final static String TAG_DATA_VERSION= "data-version";
+	public final static String TAG_DATE= "date";
+	public final static String TAG_SAVED_BY= "saved-by";
+	public final static String TAG_AUTO_GENERATED_BY= "auto-generated-by";
+	public final static String TAG_IMPORT= "import";
+	public final static String TAG_SUBSETDEF= "subsetdef";
+	public final static String TAG_SYNONYMTYPEDEF= "synonymtypedef";
+	public final static String TAG_DEFAULT_NAMESPACE= "default-namespace";
+	public final static String TAG_IDSPACE= "idspace";
+	public final static String TAG_TREAT_XREFS_AS_EQUIVALENT= "treat-xrefs-as-equivalent";
+	public final static String TAG_TREAT_XREFS_AS_GENUS_DIFFERENTIA= "treat-xrefs-as-genus-differentia";
+	public final static String TAG_TREAT_XREFS_AS_RELATIONSHIP= "treat-xrefs-as-relationship";
+	public final static String TAG_TREAT_XREFS_AS_IS_A= "treat-xrefs-as-is_a";
+	public final static String TAG_REMARK= "remark";
+	public final static String TAG_ID= "id";
+	public final static String TAG_NAME= "name";
+	public final static String TAG_NAMESPACE= "namespace";
+	public final static String TAG_ALT_ID= "alt_id";
+	public final static String TAG_DEF= "def";
+	public final static String TAG_COMMENT= "comment";
+	public final static String TAG_SUBSET= "subset";
+	public final static String TAG_SYNONYM= "synonym";
+	public final static String TAG_XREF= "xref";
+	public final static String TAG_BUILTIN= "builtin";
+	public final static String TAG_PROPERTY_VALUE= "property_value";
+	public final static String TAG_IS_A= "is_a";
+	public final static String TAG_INTERSECTION_OF= "intersection_of";
+	public final static String TAG_UNION_OF= "union_of";
+	public final static String TAG_EQUIVALENT_TO= "equivalent_to";
+	public final static String TAG_DISJOINT_FROM= "disjoint_from";
+	public final static String TAG_RELATIONSHIP= "relationship";
+	public final static String TAG_CREATED_BY= "created_by";
+	public final static String TAG_CREATION_DATE= "creation_date";
+	public final static String TAG_IS_OBSELETE= "is_obsolete";
+	public final static String TAG_REPLACED_BY= "replaced_by";
+	public final static String TAG_IS_ANONYMOUS= "is_anonymous";
+	public final static String TAG_DOMAIN= "domain";
+	public final static String TAG_RANGE= "range";
+	public final static String TAG_IS_ANTI_SYMMETRIC= "is_anti_symmetric";
+	public final static String TAG_IS_CYCLIC= "is_cyclic";
+	public final static String TAG_IS_REFLEXIVE= "is_reflexive";
+	public final static String TAG_IS_SYMMETRIC= "is_symmetric";
+	public final static String TAG_IS_TRANSITIVE= "is_transitive";
+	public final static String TAG_IS_FUNCTIONAL= "is_functional";
+	public final static String TAG_IS_INVERSE_FUNCTIONAL= "is_inverse_functional";
+	public final static String TAG_TRANSITIVE_OVER= "transitive_over";
+	public final static String TAG_HOLDS_OVER_CHAIN= "holds_over_chain";
+	public final static String TAG_EQUIVALENT_TO_CHAIN= "equivalent_to_chain";
+	public final static String TAG_DISJOINT_OVER= "disjoint_over";
+	public final static String TAG_EXPAND_ASSERTION_TO= "expand_assertion_to";
+	public final static String TAG_EXPAND_EXPRESSION_TO= "expand_expression_to";
+	public final static String TAG_IS_CLASS_LEVEL= "is_class_level";
+	
+	
+	
+}
