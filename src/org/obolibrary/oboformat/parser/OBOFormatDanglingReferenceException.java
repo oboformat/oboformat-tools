3f2963d5f38b2147dcbc3dde56b4584ee58395b1
shahid.manzoor <shahid.manzoor@3e622410-f0ad-ce59-49ea-9bf623c22f00>
2011-01-25 19:01:09 +0000

    git-svn-id: http://oboformat.googlecode.com/svn/trunk@61 3e622410-f0ad-ce59-49ea-9bf623c22f00

---
+++ src/org/obolibrary/oboformat/parser/OBOFormatDanglingReferenceException.java
@@ -0,0 +1,20 @@
+package org.obolibrary.oboformat.parser;
+
+public class OBOFormatDanglingReferenceException extends OBOFormatException {
+
+	public OBOFormatDanglingReferenceException() {
+	}
+
+	public OBOFormatDanglingReferenceException(String arg0) {
+		super(arg0);
+	}
+
+	public OBOFormatDanglingReferenceException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public OBOFormatDanglingReferenceException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+}
---
+++ src/org/obolibrary/oboformat/parser/OBOFormatException.java
@@ -0,0 +1,20 @@
+package org.obolibrary.oboformat.parser;
+
+public class OBOFormatException extends Exception {
+
+	public OBOFormatException() {
+	}
+
+	public OBOFormatException(String arg0) {
+		super(arg0);
+	}
+
+	public OBOFormatException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public OBOFormatException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+}
