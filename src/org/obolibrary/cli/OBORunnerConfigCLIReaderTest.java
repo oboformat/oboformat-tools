9a99f54c8424d2ecbc67b2b26ec4ae783025f46d
hdietze@lbl.gov <hdietze@lbl.gov@3e622410-f0ad-ce59-49ea-9bf623c22f00>
2011-06-30 20:39:25 +0000

    test '-x' parameter
    git-svn-id: http://oboformat.googlecode.com/svn/trunk@134 3e622410-f0ad-ce59-49ea-9bf623c22f00

--- src/org/obolibrary/cli/OBORunnerConfigCLIReaderTest.java
+++ src/org/obolibrary/cli/OBORunnerConfigCLIReaderTest.java
@@ -7,13 +7,13 @@ import junit.framework.TestCase;
 public class OBORunnerConfigCLIReaderTest extends TestCase {
 
 	public void testReadConfig() {
-		OBORunnerConfiguration config = OBORunnerConfigCLIReader.readConfig(new String[]{"-o","obi","http://some.url"});
+		OBORunnerConfiguration config = OBORunnerConfigCLIReader.readConfig(new String[]{"-o","obi","-x","http://some.url"});
 		assertEquals(new Boolean(true), config.isOboToOwl.getValue());
 		assertEquals("obi", config.outFile.getValue());
 		Collection<String> value = config.paths.getValue();
 		assertEquals(1, value.size());
 		assertEquals("http://some.url", value.toArray()[0]);
-		
+		assertEquals(new Boolean(true), config.isExpandMacros.getValue());
 	}
 
 }
