a5b493cad13b1b8c90f43edc24413b4f94de8a47
cmungall <cmungall@3e622410-f0ad-ce59-49ea-9bf623c22f00>
2011-07-13 20:20:00 +0000

    git-svn-id: http://oboformat.googlecode.com/svn/trunk@164 3e622410-f0ad-ce59-49ea-9bf623c22f00

--- src/org/obolibrary/obo2owl/test/RoundTripFlyAnatomyTest.java
+++ src/org/obolibrary/obo2owl/test/RoundTripFlyAnatomyTest.java
@@ -19,14 +19,15 @@ import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 
 import junit.framework.TestCase;
 
-public class RoundTripTestCL extends RoundTripTest {
+public class RoundTripFlyAnatomyTest extends RoundTripTest {
 
 	static OWLOntologyManager manager;
 	static OWLDataFactory df;
 	static OWLOntology ontology;
 
 	public static void testRoundTrip() throws IOException, OWLOntologyCreationException, OWLOntologyStorageException {
-		roundTripOBOURL("http://obo.cvs.sourceforge.net/*checkout*/obo/obo/ontology/anatomy/cell_type/cell.obo", true);		
+		roundTripOBOURL("http://obo.cvs.sourceforge.net/*checkout*/obo/obo/ontology/anatomy/gross_anatomy/animal_gross_anatomy/fly/fly_anatomy_XP.obo", true);
+		
 	}
 	
 	
