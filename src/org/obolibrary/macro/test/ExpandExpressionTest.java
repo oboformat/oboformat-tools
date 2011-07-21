601a70c782a7323b49616a0a8cf65bfc950e1009
cmungall <cmungall@3e622410-f0ad-ce59-49ea-9bf623c22f00>
2011-07-21 22:11:53 +0000

    git-svn-id: http://oboformat.googlecode.com/svn/trunk@239 3e622410-f0ad-ce59-49ea-9bf623c22f00

--- src/org/obolibrary/macro/test/ExpandExpressionTest.java
+++ src/org/obolibrary/macro/test/ExpandExpressionTest.java
@@ -16,8 +16,12 @@ import org.semanticweb.owlapi.io.OWLXMLOntologyFormat;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLDisjointClassesAxiom;
+import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
+import org.semanticweb.owlapi.model.OWLObjectIntersectionOf;
+import org.semanticweb.owlapi.model.OWLObjectSomeValuesFrom;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyFormat;
@@ -32,9 +36,9 @@ public class ExpandExpressionTest extends TestCase {
 	public static void testExpand() throws IOException, OWLOntologyCreationException, OWLOntologyStorageException {
 		Logger.getRootLogger().setLevel(Level.ALL);
 		OWLOntology owlOnt = convertOBOFile("no_overlap.obo");
-		
+
 	}
-	
+
 	public static OBODoc parseOBOFile(String fn) throws IOException {
 		OBOFormatParser p = new OBOFormatParser();
 		OBODoc obodoc = p.parse("test_resources/"+fn);
@@ -50,21 +54,40 @@ public class ExpandExpressionTest extends TestCase {
 		OWLOntologyManager manager = bridge.getManager();
 		OWLDataFactory df = manager.getOWLDataFactory();
 		OWLOntology ontology = bridge.convert(obodoc);
-		
+
 		MacroExpansionVisitor mev = 
 			new MacroExpansionVisitor(df,ontology, manager);
 		OWLOntology outputOntology = mev.expandAll();
-		
+
 		OWLClass cls = df.getOWLClass(IRI.create("http://purl.obolibrary.org/obo/TEST_2"));
 		Set<OWLDisjointClassesAxiom> dcas = outputOntology.getDisjointClassesAxioms(cls);
 		System.out.println(dcas);
 		assertTrue(dcas.size() == 1);
-		
+
 		cls = df.getOWLClass(IRI.create("http://purl.obolibrary.org/obo/TEST_3"));
 		Set<OWLSubClassOfAxiom> scas = outputOntology.getSubClassAxiomsForSubClass(cls);
 		System.out.println(scas);
 		assertTrue(scas.size() == 1);
 		assertTrue(scas.toString().equals("[SubClassOf(<http://purl.obolibrary.org/obo/TEST_3> ObjectSomeValuesFrom(<http://purl.obolibrary.org/obo/BFO_0000051> ObjectIntersectionOf(<http://purl.obolibrary.org/obo/GO_0005886> ObjectSomeValuesFrom(<http://purl.obolibrary.org/obo/BFO_0000051> <http://purl.obolibrary.org/obo/TEST_4>))))]"));
+
+		cls = df.getOWLClass(IRI.create("http://purl.obolibrary.org/obo/TEST_4"));
+		Set<OWLEquivalentClassesAxiom> ecas = outputOntology.getEquivalentClassesAxioms(cls);
+		boolean ok = false;
+		for (OWLEquivalentClassesAxiom eca : ecas) {
+			for (OWLClassExpression x : eca.getClassExpressions()) 
+				if (x instanceof OWLObjectIntersectionOf) {
+					for (OWLClassExpression y : ((OWLObjectIntersectionOf) x).getOperands()) {
+						if (y instanceof OWLObjectSomeValuesFrom) {
+							String pStr = ((OWLObjectSomeValuesFrom)y).getProperty().toString();
+							System.out.println("p="+pStr);
+							assertTrue(pStr.equals("<http://purl.obolibrary.org/obo/BFO_0000051>"));
+							ok=true;
+						}
+					}
+				}
+		}
+		assertTrue(ok);
+
 
 
 		IRI outputStream = IRI.create("file:///tmp/"+fn+".owl");
